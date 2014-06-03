64d1cace6fe70075592c8f63c40325a2f65b5f6f
Liz Tom <liz.tom@gmail.com>
2014-06-03 14:29:18 -0700

    change prettyName function

--- app/cxs.js
+++ app/cxs.js
@@ -329,7 +329,7 @@ function prettySize(size){
 }
 
 function prettyName(name) {
-  return (name.length > MAX_NAME_LENGTH) ? name.substring(0, 28) + "...";
+  return (name.length > MAX_NAME_LENGTH) ? name.substring(0, MAX_NAME_LENGTH-3) + "...";
 }
 
 app.get('/api/azure/upload', function(req,res){
